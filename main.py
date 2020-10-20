37dd7850510ffabf60e45f96ba23de6618f7ac06
Vitor <vbardasson@id.uff.br>
2020-10-20 16:20:15 -0300

    Criação de lista de dicts

--- main.py
+++ main.py
@@ -8,14 +8,17 @@ def main():
     #leitura dos processos no arquivo
     with open('../processes.txt') as reader:
         for line in reader.readlines():
-            p.append(line.split(', '))
-            #inputInfo[0] -> arrivalTime
-            #inputInfo[1] -> priority 
-            #inputInfo[2] -> CPU time
-            #inputInfo[3] -> MBytes
-            #inputInfo[4] -> printers
-            #inputInfo[5] -> disk
-    sort_p = p.sort(key = lambda x: x[0]) #adicionar a vetor auxiliar ordenado p/ ordem de chegada do processo
+            arrivalTime, priority, serviceTime, size, printers, disk = line.split(', ')
+            newInput = {
+                "arrivalTime": arrivalTime,
+                "priority": priority,
+                "serviceTime": serviceTime,
+                "size": size,
+                "printers": printers,
+                "disk": disk
+            }
+            p.append(newInput)
+    sort_p = p.sort(key = lambda x: x['arrivalTime']) #adicionar a vetor auxiliar ordenado p/ ordem de chegada do processo
     loop(system.memory, sort_p)
             
 def loop(memory, sort_p):
@@ -30,7 +33,7 @@ def loop(memory, sort_p):
             memory.criticalProcesses[0].state = RUNNING
             for rq in memory.rq:
                 if (rq[0]): #caso a fila não esteja vazia
-                    rq[0].state = READY
+                    rq[0].state = ProcessState.READY
                     #mover o processo corrente de fila?
 
         #chegada de processo no sistema
@@ -38,7 +41,7 @@ def loop(memory, sort_p):
             #priority = 0 -> critical
             #prority = 1 -> user
             processInput = sort_p.pop(0)
-            newProcess = dispatcher.createProcess(processInput[0], processInput[1], processInput[2], processInput[3], processInput[4], processInput[5])
+            newProcess = dispatcher.createProcess(processInput['arrivalTime'], processInput['priority'], processInput['serviceTime'], processInput['size'], processInput['printers'], processInput['disk'])
             dispatcher.addNewToQueue(newProcess, memory)
 
         if not ("""current_time""" % QUANTUM):
